8cd3b54bbe0b66f6f8ff4d430f309bf99a3e2a98
m00head <m00head@yahoo.co.uk>
2016-05-06 11:22:28 +0100

    Update to Vulkan API version 1.0.18

--- external/vulkan/spirv.hpp
+++ external/vulkan/spirv.hpp
@@ -47,11 +47,11 @@ namespace spv {
 typedef unsigned int Id;
 
 #define SPV_VERSION 0x10000
-#define SPV_REVISION 4
+#define SPV_REVISION 5
 
 static const unsigned int MagicNumber = 0x07230203;
 static const unsigned int Version = 0x00010000;
-static const unsigned int Revision = 4;
+static const unsigned int Revision = 5;
 static const unsigned int OpCodeMask = 0xffff;
 static const unsigned int WordCountShift = 16;
 
@@ -220,6 +220,7 @@ enum ImageChannelOrder {
     ImageChannelOrdersRGBx = 16,
     ImageChannelOrdersRGBA = 17,
     ImageChannelOrdersBGRA = 18,
+    ImageChannelOrderABGR = 19,
 };
 
 enum ImageChannelDataType {
