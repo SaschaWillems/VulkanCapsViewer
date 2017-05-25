4988be5ab82c1a7d03db1ce4a3554de0528a0d3b
saschawillems <webmaster@saschawillems.de>
2017-05-25 09:14:25 +0200

    Check QT-Version >= 5.6 before enabling high dpi scaling

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 #include <QDebug>
 #include <QCommandLineParser>
 #include <QApplication>
+#include <QtGlobal>
 
 int main(int argc, char *argv[])
 {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 
     QApplication a(argc, argv);
     a.setApplicationVersion(QString::fromStdString(vulkanCapsViewer::version));
