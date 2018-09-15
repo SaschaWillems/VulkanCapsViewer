52a6a9bf9ea69989a20780312f1bcabc32b77ff5
saschawillems <webmaster@saschawillems.de>
2018-09-15 09:58:17 +0200

    Version bump

--- vulkancapsviewer.cpp
+++ vulkancapsviewer.cpp
@@ -63,7 +63,7 @@
 
 #define VK_API_VERSION VK_API_VERSION_1_1
 
-const std::string vulkanCapsViewer::version = "1.7";
+const std::string vulkanCapsViewer::version = "1.8";
 
 /// <summary>
 ///	Returns operating system name
