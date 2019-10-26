3db554165af9b860dca98a216742d2d7df9a1bf0
Sascha Willems <webmaster@saschawillems.de>
2019-10-26 09:54:40 +0200

    Added translation for VK_PHYSICAL_DEVICE_TYPE_CPU
    Refs #76

--- vulkanresources.h
+++ vulkanresources.h
@@ -50,6 +50,7 @@ public:
                 STR(INTEGRATED_GPU);
                 STR(DISCRETE_GPU);
                 STR(VIRTUAL_GPU);
+                STR(CPU);
 #undef STR
 default: return "UNKNOWN_DEVICE";
 		}
