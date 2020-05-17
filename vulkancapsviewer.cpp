cfe8798f7a0503b4c49285054cd6c597149acda0
Sascha Willems <webmaster@saschawillems.de>
2020-05-17 18:42:38 +0200

    Version bump

--- vulkancapsviewer.cpp
+++ vulkancapsviewer.cpp
@@ -69,7 +69,7 @@ using std::to_string;
 
 #define VK_API_VERSION VK_API_VERSION_1_1
 
-const std::string vulkanCapsViewer::version = "2.1";
+const std::string vulkanCapsViewer::version = "2.2";
 const std::string vulkanCapsViewer::reportVersion = "2.0";
 
 /// <summary>
