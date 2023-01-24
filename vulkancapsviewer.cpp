f28e0b8616627d089a9afb72041eea05ec6a6458
Sascha Willems <webmaster@saschawillems.de>
2023-01-24 18:43:00 +0100

    Version bump

--- vulkancapsviewer.cpp
+++ vulkancapsviewer.cpp
@@ -77,7 +77,7 @@ extern "C" const char *getWorkingFolderForiOS(void);
 
 using std::to_string;
 
-const QString VulkanCapsViewer::version = "3.27";
+const QString VulkanCapsViewer::version = "3.28";
 const QString VulkanCapsViewer::reportVersion = "3.2";
 
 OSInfo getOperatingSystem()
