043f64224bc59b484e2ea860ee83344618d12653
Sascha Willems <webmaster@saschawillems.de>
2023-04-22 08:44:45 +0200

    Remove display of surface min and max extents
    These values are tied to the window size and not relevant for the database
    Still fetched and stored to the JSON for compatibility reasons
    Fixes #27

--- vulkancapsviewer.cpp
+++ vulkancapsviewer.cpp
@@ -1679,8 +1679,6 @@ void VulkanCapsViewer::displayDeviceSurfaceInfo(VulkanDeviceInfo &device)
     addTreeItem(surfaceCapsItem, "minImageCount", to_string(surfaceCaps.minImageCount));
     addTreeItem(surfaceCapsItem, "maxImageCount", to_string(surfaceCaps.maxImageCount));
     addTreeItem(surfaceCapsItem, "maxImageArrayLayers", to_string(surfaceCaps.maxImageArrayLayers));
-    addTreeItem(surfaceCapsItem, "minImageExtent", to_string(surfaceCaps.minImageExtent.width) + " x " + to_string(surfaceCaps.minImageExtent.height));
-    addTreeItem(surfaceCapsItem, "maxImageExtent", to_string(surfaceCaps.maxImageExtent.width) + " x " + to_string(surfaceCaps.maxImageExtent.height));
     addTreeItemFlags(surfaceCapsItem, "Supported usage flags", surfaceCaps.supportedUsageFlags, imageUsageBitString)->setExpanded(true);
     addTreeItemFlags(surfaceCapsItem, "Supported transforms", surfaceCaps.supportedTransforms, surfaceTransformBitString) ->setExpanded(true);
     addTreeItemFlags(surfaceCapsItem, "Composite alpha flags", surfaceCaps.supportedCompositeAlpha, compositeAlphaBitString) ->setExpanded(true);
