f909055d4921948bde2160cf8b23e8edb8c5e703
Sascha Willems <webmaster@saschawillems.de>
2023-08-06 11:50:14 +0200

    Updated Vulkan Headers to 1.3.261

--- VulkanDeviceInfoExtensions.h
+++ VulkanDeviceInfoExtensions.h
@@ -91,7 +91,7 @@ private:
     void readPhysicalFeatures_VALVE();
 
 public:
-    const uint32_t vkHeaderVersion = 260;
+    const uint32_t vkHeaderVersion = 261;
     std::vector<Feature2> features2;
     std::vector<Property2> properties2;
     std::vector<VkExtensionProperties> extensions;
