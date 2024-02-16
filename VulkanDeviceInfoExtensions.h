60884e932f1c71aa1229680bccb295854d7f16f8
Sascha Willems <webmaster@saschawillems.de>
2024-02-16 17:21:46 +0100

    Updated to Vulkan headers 1.3.278
    Adds support for:
    - VK_EXT_map_memory_placed
    - VK_NV_shader_atomic_float16_vector

--- VulkanDeviceInfoExtensions.h
+++ VulkanDeviceInfoExtensions.h
@@ -95,7 +95,7 @@ private:
     void readPhysicalFeatures_VALVE();
 
 public:
-    const uint32_t vkHeaderVersion = 276;
+    const uint32_t vkHeaderVersion = 278;
     std::vector<Feature2> features2;
     std::vector<Property2> properties2;
     std::vector<VkExtensionProperties> extensions;
