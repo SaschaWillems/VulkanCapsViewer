dee3df78170f3929ee2d7b21611c80bae76d3bc0
Sascha Willems <webmaster@saschawillems.de>
2024-12-02 18:43:11 +0100

    Add support for
    - VK_EXT_vertex_attribute_robustness
    - VK_KHR_video_encode_av1
    - VK_KHR_video_encode_quantization_map

--- VulkanDeviceInfoExtensions.h
+++ VulkanDeviceInfoExtensions.h
@@ -89,7 +89,7 @@ private:
     void readPhysicalFeatures_VALVE();
 
 public:
-    const uint32_t vkHeaderVersion = 301;
+    const uint32_t vkHeaderVersion = 303;
     std::vector<Feature2> features2;
     std::vector<Property2> properties2;
     std::vector<VkExtensionProperties> extensions;
