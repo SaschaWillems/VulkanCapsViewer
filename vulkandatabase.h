80dc9b34414daab04fb4578c82bef4499e7d9978
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-06-01 05:20:49 +0900

    fix: avoid `using namespace std;` in header file
    using directive in a header file is a bad practice because it may lead
    to unexpected results.
    
    https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rs-using-directive

--- vulkandatabase.h
+++ vulkandatabase.h
@@ -32,8 +32,6 @@
 
 #include "vulkanDeviceInfo.h"
 
-using namespace std;
-
 class VulkanDatabase :
 	public QObject
 {
@@ -41,8 +39,8 @@ class VulkanDatabase :
 private:
 	QNetworkProxy *proxy;
 	QNetworkAccessManager *manager;
-	string httpGet(string url);
-	string encodeUrl(string url);
+	std::string httpGet(std::string url);
+	std::string encodeUrl(std::string url);
 public:
     static QString username;
     static QString password;
