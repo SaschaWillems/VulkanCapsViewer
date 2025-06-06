b51365239839d1c11e38cc4433e188bd737f64d8
Sascha Willems <webmaster@saschawillems.de>
2025-06-06 14:15:06 +0200

    Add linux aarch64 releases

--- tools/download_releases.php
+++ tools/download_releases.php
@@ -10,8 +10,10 @@ function download($remote, $local) {
     }
 }
 
-$version = "3.43";
+$version = "4.01";
 $name_template = "vulkancapsviewer_".$version."_%s";
+
+echo "Starting downloads for $name_template ...".PHP_EOL;
 
 # Windows 64 bit
 download('Vulkan_Caps_Viewer-windows.zip', sprintf($name_template, 'win64.zip'));
@@ -21,9 +23,14 @@ download('Vulkan_Caps_Viewer-windows_x86.zip', sprintf($name_template, 'winx86.z
 
 # Linux X11
 download('Vulkan_Caps_Viewer-X11-x86_64.AppImage', sprintf($name_template, 'linux64.AppImage'));
+download('Vulkan_Caps_Viewer-X11-aarch64.AppImage', sprintf($name_template, 'linux64_aarch.AppImage'));
 
 # Linux Wayland
 download('Vulkan_Caps_Viewer-wayland-x86_64.AppImage', sprintf($name_template, 'linux64_wayland.AppImage'));
+download('Vulkan_Caps_Viewer-wayland-aarch64.AppImage', sprintf($name_template, 'linux64_wayland_aarch.AppImage'));
 
 # OSX
 download('Vulkan_Caps_Viewer-osx-x86_64.dmg', sprintf($name_template, 'osx.dmg'));
+
+# Android is compiled locally
+copy('android-build-release-signed.apk', sprintf($name_template, 'arm.apk'));
